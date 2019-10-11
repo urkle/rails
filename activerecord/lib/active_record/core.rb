61e89c2179555ff9374f3b3692b536505c7a598e
Edward Rudd <urkle@outoforder.cc>
2019-10-11 17:04:37 -0400

    Allow skipping ActiveModel::Dirty#initialize_dup's attribute cloning
    This is the case when ActiveRecord::Core#initialize_dup also clones the attributes.
    
    TODO - fix the "changed" nature of the attributes

--- activerecord/lib/active_record/core.rb
+++ activerecord/lib/active_record/core.rb
@@ -421,6 +421,7 @@ module ActiveRecord
     def initialize_dup(other) # :nodoc:
       @attributes = @attributes.deep_dup
       @attributes.reset(@primary_key)
+      @skip_dup_attributes = true
 
       _run_initialize_callbacks
 
